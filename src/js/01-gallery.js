a97a3c4356d2a98ccfef59759a5a59904f8695de
AliVik <alvikul1994@gmail.com>
2021-10-29 22:00:46 +0300

    копирует код предыдущей дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = createMarkup(galleryItems);
+gallery.addEventListener('click', onImgClick);
+
+function createMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item"><a href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+    }).join('');
+}
+
+
+function onImgClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+}
+
+
+new SimpleLightbox('.gallery a',
+    {
+        captionsData: "alt",
+        captionPosition: 'bottom',
+        captionDelay: '250',
+        focus: false,
+        
+    });
