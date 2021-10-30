413c69dc7375432cc27ce801f0c0a269945a7b24
AliVik <alvikul1994@gmail.com>
2021-10-30 20:08:55 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,7 @@ const player = new Player(iframe);
 player.on('timeupdate', _.throttle((evt) => {
     
     localStorage.setItem("videoplayer-current-time", `${evt.seconds}`);
-    
+    console.log(localStorage['videoplayer-current-time'])
   }, 1000))
 
 player.setCurrentTime(localStorage['videoplayer-current-time']);
