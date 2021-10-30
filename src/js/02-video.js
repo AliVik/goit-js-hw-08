fbe1a23b029d63f43fd5d1aa9b019fe5240f2a3c
AliVik <alvikul1994@gmail.com>
2021-10-30 20:00:32 +0300

    подключает видео-библиотеку, библиотеку lodash, настраивает функции throttle и перезагрузки

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', _.throttle((evt) => {
+    
+    localStorage.setItem("videoplayer-current-time", `${evt.seconds}`);
+    
+  }, 1000))
+
+player.setCurrentTime(localStorage['videoplayer-current-time']);
