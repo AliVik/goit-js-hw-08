12830721a14dc36d4de06e521c3e6a6813031a28
AliVik <alvikul1994@gmail.com>
2021-10-30 21:45:01 +0300

    подключает библиотеку, добавляет код для формы

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import _ from 'lodash';
 
+const formRef = document.querySelector('.feedback-form');
+const emailInput = document.querySelector("input[name='email']");
+const textareaInput = document.querySelector("textarea[name='message']");
+
+
+formRef.addEventListener('input', _.throttle(() => {
+ const feedback = {
+        email: `${emailInput.value}`,
+        message: `${textareaInput.value}`,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+   storageCheck()
+    
+}, 500));
+
+console.log(localStorage)
+
+function storageCheck() {
+    if (localStorage.length !== 0) {
+         emailInput.value = localStorage.getItem('feedback[email]');
+        textareaInput.value = localStorage.getItem('feedback[message]');
+    } else {
+        emailInput.value = '';
+        textareaInput.value = '';
+    }
+}  
+   
+
+// }
